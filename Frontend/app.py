92387b41d91fe3d7f1bb09807bc2d8b2ee91dfa3
ThirumalJeegari <thirumaljeegari21@gmail.com>
2026-06-07 19:07:27 +0530

    initial commit

--- Frontend/app.py
+++ Frontend/app.py
@@ -2,84 +2,41 @@ import streamlit as st
 import requests
 import json
 import pandas as pd
+S_URL = "http://127.0.0.1:8000"
 
-S_URL = "https://single-agent-with-multi-tools.onrender.com"
-
-
-
-def safe_json_response(res):
-    try:
-        return res.json()
-    except Exception:
-        st.error("Backend did not return JSON")
-        st.write("Status Code:", res.status_code)
-        st.write("Backend Response:")
-        st.code(res.text)
-        st.stop()
-
-w_tab, s_tab, web_tab = st.tabs([
-    "Weather Tool",
-    "SQL Tool",
-    "Web Tool"
+w_tab ,s_tab, web_tab=st.tabs([
+    "Weather_Tool","SQL Tool","Web Tool"
 ])
 
-
-
 with web_tab:
-
-    st.title("🌐 AI Web Search Agent")
-
-    question = st.text_input(
-        "Ask a general question"
-    )
+    question=st.text_input("Ask a general question ")
 
     if st.button("GetGenData"):
+        res=requests.post(f"{S_URL}/web_tool_calling",params={
+            "question":question
+        })
 
-        res = requests.post(
-            f"{S_URL}/web_tool_calling",
-            params={
-                "question": question
-            }
-        )
+        st.write(res)
+        
 
-        obj = safe_json_response(res)
 
-        try:
-            answer = obj["messages"][-1]["content"]
-            st.write("### Web Search Result")
-            st.write(answer)
+with s_tab:
+    question=st.text_input("Ask a sql question ")
 
-        except Exception as e:
-            st.error(f"Error : {e}")
-            st.json(obj)
+    if st.button("GetData"):
+        res=requests.post(f"{S_URL}/sql_tool_calling",params={
+            "question":question
+        })
 
-            
-@app.get("/test_db")
-def test_db():
-    try:
-        con_obj = get_db_connection()
-        cursor = con_obj.cursor(dictionary=True)
-
-        cursor.execute("SELECT COUNT(*) AS total_employees FROM employees")
-        result = cursor.fetchone()
-
-        cursor.close()
-        con_obj.close()
-
-        return {
-            "status": "success",
-            "data": result
-        }
-
-    except Exception as e:
-        return {
-            "status": "failed",
-            "error": str(e)
-        }
-
+        obj=res.json()
+        messages=obj["messages"][2]["content"]
+        emps = json.loads(messages)
+        st.write(emps)
+        df=pd.DataFrame(emps)
+        st.dataframe(df)
 
 with w_tab:
-
+    
     st.title("🌤 AI Weather Agent")
 
     city = st.text_input(
@@ -91,27 +48,16 @@ with w_tab:
     )
 
     if st.button("Ask Agent"):
+        res=requests.post(f"{S_URL}/tool_calling",params={
+            "city":city,
+            "question":question
+        })
+        objRes=res.json()
+        messages=objRes["messages"][2]["content"]
+        obj=json.loads(messages)
+        st.write(obj["main"])
+        # st.write(messages)
+        # st.write(res.json()["messages"][2]["content"])
+        st.write(res.json()["messages"][3]["content"])
 
-        res = requests.post(
-            f"{S_URL}/tool_calling",
-            params={
-                "city": city,
-                "question": question
-            }
-        )
-
-        objRes = safe_json_response(res)
-
-        
-        messages = objRes["messages"][2]["content"]
-
-        obj = json.loads(messages)
-
-        st.write("### Weather Details")
-
-        st.write(
-            f"Temperature is {obj['main']['temp']}°C. "
-            f"It feels like {obj['main']['feels_like']}°C. "
-            f"Humidity is {obj['main']['humidity']}%. "
-            f"Pressure is {obj['main']['pressure']} hPa."
-        )
+        # st.success(res.json()["messages"][-1]["content"])
